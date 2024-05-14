b46d1fd2601fa36074d6f7abd8ae9ae428c741a7
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-14 12:55:16 +0700

    Should return 1 when input 1

---
+++ fizzbuzz_test.go
@@ -0,0 +1,16 @@
+package kata
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	t.Run("should return 1 when input 1", func(t *testing.T) {
+		input := 1
+		want := "1"
+
+		get := fizzbuzz(input)
+
+		if get != want {
+			t.Errorf("should return %s when input %s", want, get)
+		}
+	})
+}
