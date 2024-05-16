5c4bf84d2d4dc5bb83e371b6f531a25473e56a7b
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-16 07:20:05 +0700

    Should return 11 when input 11

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func fizzbuzz(n int) string {
     if n%5 == 0 {
-        return "buzz"
-    }
-
-    if n%3 == 0 {
-    	return "fizz"
-    }
-
-    return fmt.Sprint(n)
+		return "buzz"
+	}
+ 
+	if n%3 == 0 {
+		return "fizz"
+	}
+ 
+	return fmt.Sprint(n)
 }
