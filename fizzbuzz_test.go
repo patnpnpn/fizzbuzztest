75b8df55c1fc0b01d9129b86908e07c355a6dfdb
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-16 08:26:04 +0700

    Should return buzz when input 5

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -46,4 +46,15 @@ func TestFizzBuzz(t *testing.T) {
             t.Errorf("should return %s when input %s", want, get)
         }
     })
+    
+    t.Run("should return buzz when input 5", func(t *testing.T) {
+        input := 5
+        want := "buzz"
+
+        get := fizzbuzz(input)
+
+        if get != want {
+            t.Errorf("should return %s when input %s", want, get)
+        }
+    })
 }
