c1ef97cc084df893b0ff98db8c39f73fe7bd52cd
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-16 08:15:50 +0700

    Should return 1 when input 1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,19 +1,5 @@
 package kata
 
-import "fmt"
-
 func fizzbuzz(n int) string {
-    if n%15 == 0 {
-		return "fizzbuzz"
-	}
- 
-    if n%5 == 0 {
-		return "buzz"
-	}
- 
-	if n%3 == 0 {
-		return "fizz"
-	}
- 
-	return fmt.Sprint(n)
+    return "1"
 }
