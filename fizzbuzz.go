ef86217564b96596d047c34f46761234fc07d181
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-16 08:30:09 +0700

    Refactor when divisible by 3

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,17 +3,13 @@ package kata
 import "fmt"
 
 func fizzbuzz(n int) string {
-    if n == 6 {
-		return "fizz"
-	}
- 
-	if n == 5 {
-		return "buzz"
-	}
- 
-	if n == 3 {
-		return "fizz"
-	}
- 
-	return fmt.Sprint(n)
+    if n == 5 {
+        return "buzz"
+    }
+
+    if n%3 == 0 {
+        return "fizz"
+    }
+
+    return fmt.Sprint(n)
 }
