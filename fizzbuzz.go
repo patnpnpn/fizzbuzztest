4b3da3f043492ead91c87e84b5b20ffcc1853559
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-16 08:31:59 +0700

    Should return 7 when input 7

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,13 +3,13 @@ package kata
 import "fmt"
 
 func fizzbuzz(n int) string {
-    if n == 5 {
-        return "buzz"
-    }
-
-    if n%3 == 0 {
-        return "fizz"
-    }
-
-    return fmt.Sprint(n)
+     if n == 5 {
+		return "buzz"
+	}
+ 
+	if n%3 == 0 {
+		return "fizz"
+	}
+ 
+	return fmt.Sprint(n)
 }
