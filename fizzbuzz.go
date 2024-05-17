372e816bff86fa45ce7fe8a2c354e334308c29c9
Patsornchai Noppakun <patsornchai.n@arise.tech>
2024-05-17 10:06:06 +0700

    Should return buzz when input 5

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -10,10 +10,18 @@ func isEqualToThree(n int) bool {
 	return n == 3
 }
 
+func isEqualToFive(n int) bool {
+	return n == 5
+}
+
 func fizzbuzz(n int) string {
 	if isEqualToThree(n) {
 		return "fizz"
 	}
-	
+
+	if isEqualToFive(n) {
+		return "buzz"
+	}
+
 	return convertIntToString(n)
 }
